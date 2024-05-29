0b7bfc824b8fdcdfaa6cedeb69a7ae80f8c1e67b
keschler <keschler@duck.com>
2024-05-29 12:44:14 +0200

    Dynamic username change in posts, minor CSS fixes, and OOP implementation in backend.py" - Conducted minor CSS adjustments - Incorporated Object-Oriented Programming principles in backend.py - Updated username in posts when user modifies their nam

--- backend.py
+++ backend.py
@@ -5,74 +5,12 @@ import bcrypt
 from pymongo import MongoClient
 
 cluster = MongoClient(
-    "mongodb+srv://spadabailu:T94jkJaEAooyLhnhicmLhWTRVi3mu9X3WPDxWfDfWACcKjMeWs@cluster0.fzgybta.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    "mongodb+srv://spadabailu:T94jkJaEAooyLhnhicmLhWTRVi3mu9X3WPDxWfDfWACcKjMeWs@cluster0.fzgybta.mongodb.net"
+    "/?retryWrites=true&w=majority&appName=Cluster0")
 db = cluster["twitter"]
 
 
-def register_user(username, password):
-    description = ''
-    if not input_validation(username):
-        return False
-    if db["users"].find_one({"username": username}) is None:  # If there is no user with the same name
-        hashed_password = bcrypt.hashpw(
-            password.encode('utf-8'), bcrypt.gensalt())
-        db["users"].insert_one({"_id": db["users"].count_documents(
-            {}) + 1, "username": username, "password": hashed_password, "description": description, "posts": [],
-                                "liked_posts": [],
-                                "retweeted_posts": []})
-        return True
-    else:
-        return False
-
-
-def edit_profile(username_old, username_new, description):
-    if db["users"].find_one({"username": username_new}) is None:
-        db["users"].update_one({"username": username_old}, {"$set": {"description": description, "username": username_new}})
-        return True
-    return False
-
-
-def login_user(username, password):
-    if not input_validation(username):
-        return False
-    user_info = db["users"].find_one({"username": username})
-    if user_info and bcrypt.checkpw(password.encode("utf-8"), user_info["password"]):
-        return True
-    else:
-        return False
-
-
-def get_description(username):
-    try:
-        description = db["users"].find_one({"username": username})["description"]
-    except TypeError:
-        return "TypeError"
-    return description
-
-
-def get_all_posts():
-    posts = db["posts"].find().sort("_id", -1)
-    return list(posts)
-
-
-def get_user_posts(username):
-    try:
-        user_posts = db["users"].find_one({"username": username})["posts"]
-    except TypeError:
-        return "TypeError"
-    reversed_user_posts = user_posts[::-1]  # Reverse the list to show the latest post first
-    return reversed_user_posts
-
-
-def get_one_post(post_id):
-    try:
-        user_posts = db["posts"].find_one({"_id": int(post_id)})
-    except TypeError:
-        return "TypeError"
-    return user_posts
-
-
-def input_validation(username):
+def input_validation(username: str):
     username_pattern = re.compile("^[a-zA-Z0-9]+$")
     if username_pattern.match(username):
         return True
@@ -80,68 +18,142 @@ def input_validation(username):
         return False
 
 
-def upload_message(user, content):
-    post = {
-        "_id": db["posts"].count_documents({}) + 1,
-        "user": user,
-        "content": content,
-        "likes": 0,
-        "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "retweets": 0,
-        "liked_by": [],
-        "retweeted_by": [],
-        "comments": []
-    }
-    db["posts"].insert_one(post)
-    db["users"].update_one({"username": user}, {"$push": {"posts": post}})
-    return None
+class User:
+    def __init__(self, username: str):
+        self.username = username
+
+    def register(self, password: str):
+        description = ''
+        if not input_validation(self.username):
+            return False
+        if db["users"].find_one({"username": self.username}) is None:  # If there is no user with the same name
+            hashed_password = bcrypt.hashpw(
+                password.encode('utf-8'), bcrypt.gensalt())
+            db["users"].insert_one({"_id": db["users"].count_documents(
+                {}) + 1, "username": self.username, "password": hashed_password, "description": description,
+                                    "posts": [],
+                                    "liked_posts": [],
+                                    "retweeted_posts": []})
+            return True
+        else:
+            return False
+
+    def login(self, password: str):
+        if not input_validation(self.username):
+            return False
+        user_info = db["users"].find_one({"username": self.username})
+        if user_info and bcrypt.checkpw(password.encode("utf-8"), user_info["password"]):
+            return True
+        else:
+            return False
+
+    def get_description(self):
+        try:
+            description = db["users"].find_one({"username": self.username})["description"]
+        except TypeError:
+            return "TypeError"
+        return description
+
+    @staticmethod
+    def get_username(id):
+        username = db["users"].find_one({"_id": id})["username"]
+        return username
+
+    def edit_profile(self, username_new: str, description: str):
+        if db["users"].find_one({"username": username_new}) is None:
+            db["users"].update_one({"username": self.username},
+                                   {"$set": {"description": description, "username": username_new}})
+            return True
+        return False
 
 
-def add_comment(post_id, username, content, post_creator):
-    print(post_id, username, content, post_creator)
-    int_post_id = int(post_id)
-    comment = {"comments": {"user": username, "content": content,
-                            "date": datetime.now().strftime(
-                                "%Y-%m-%d %H:%M:%S")}}
-    db["posts"].update_one({"_id": int_post_id}, {"$push": comment})
-    db["users"].update_one({"username": post_creator, "posts._id": int_post_id},
-                           {"$push": {"posts.$.comments": comment}})
+class Post:
+    def __init__(self, post_id: int = None):
+        self.post_id = post_id
 
+    @staticmethod
+    def get_all():
+        posts = db["posts"].find().sort("_id", -1)
+        return list(posts)
 
-def update_likes(post_id, username):
-    int_post_id = int(post_id)
-    post = db["posts"].find_one({"_id": int_post_id})
+    def get_one(self):
+        try:
+            user_posts = db["posts"].find_one({"_id": int(self.post_id)})
+        except TypeError:
+            return "TypeError"
+        return user_posts
 
-    if post is None:
-        return "Post not found."
-    if username not in post.get("liked_by", []):
-        db["posts"].update_one({"_id": int_post_id}, {"$inc": {"likes": 1}, "$push": {"liked_by": username}})
-        db["users"].update_one({"username": username, "posts._id": int_post_id},
-                               {"$inc": {"posts.$.likes": 1}, "$push": {"liked_posts": int_post_id}})
-    else:
-        db["posts"].update_one({"_id": int_post_id}, {"$inc": {"likes": -1}, "$pull": {"liked_by": username}})
-        db["users"].update_one({"username": username, "posts._id": int_post_id},
-                               {"$inc": {"posts.$.likes": -1}, "$pull": {"liked_posts": int_post_id}})
-    return None
+    @staticmethod
+    def get_user(username: str):
+        try:
+            user_posts = db["users"].find_one({"username": username})["posts"]
+        except TypeError:
+            return "TypeError"
+        reversed_user_posts = user_posts[::-1]  # Reverse the list to show the latest post first
+        return reversed_user_posts
 
+    @staticmethod
+    def upload_message(username: str, content: str):
+        user_id = db["users"].find_one({"username": username})["_id"]
+        post = {
+            "_id": db["posts"].count_documents({}) + 1,
+            "user_id": user_id,
+            "content": content,
+            "likes": 0,
+            "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "retweets": 0,
+            "liked_by": [],
+            "retweeted_by": [],
+            "comments": []
+        }
+        db["posts"].insert_one(post)
+        db["users"].update_one({"username": username}, {"$push": {"posts": post}})
+        return None
 
-def update_retweets(post_id, username):
-    int_post_id = int(post_id)
-    post = db["posts"].find_one({"_id": int_post_id})
+    def add_comment(self, username: str, content: str, post_creator: str):
+        print(self.post_id, username, content, post_creator)
+        int_post_id = int(self.post_id)
+        comment = {"comments": {"user": username, "content": content,
+                                "date": datetime.now().strftime(
+                                    "%Y-%m-%d %H:%M:%S")}}
+        db["posts"].update_one({"_id": int_post_id}, {"$push": comment})
+        db["users"].update_one({"username": post_creator, "posts._id": int_post_id},
+                               {"$push": {"posts.$.comments": comment}})
 
-    if post is None:
-        return "Post not found."
+    def update_likes(self, username: str):
+        int_post_id = int(self.post_id)
+        post = db["posts"].find_one({"_id": int_post_id})
 
-    if username not in post.get("retweeted_by", []):
-        db["posts"].update_one({"_id": int_post_id}, {"$inc": {"retweets": 1}, "$push": {"retweeted_by": username}})
-        db["users"].update_one(
-            {"username": username, "posts._id": int_post_id},
-            {"$inc": {"posts.$.retweets": 1}, "$push": {"retweeted_posts": int_post_id}}
-        )
-    else:
-        db["posts"].update_one({"_id": int_post_id}, {"$inc": {"retweets": -1}, "$pull": {"retweeted_by": username}})
-        db["users"].update_one(
-            {"username": username, "posts._id": int_post_id},
-            {"$inc": {"posts.$.retweets": -1}, "$pull": {"retweeted_posts": int_post_id}}
-        )
-    return None
+        if post is None:
+            return "Post not found."
+        if username not in post.get("liked_by", []):
+            db["posts"].update_one({"_id": int_post_id}, {"$inc": {"likes": 1}, "$push": {"liked_by": username}})
+            db["users"].update_one({"username": username, "posts._id": int_post_id},
+                                   {"$inc": {"posts.$.likes": 1}, "$push": {"liked_posts": int_post_id}})
+        else:
+            db["posts"].update_one({"_id": int_post_id}, {"$inc": {"likes": -1}, "$pull": {"liked_by": username}})
+            db["users"].update_one({"username": username, "posts._id": int_post_id},
+                                   {"$inc": {"posts.$.likes": -1}, "$pull": {"liked_posts": int_post_id}})
+        return None
+
+    def update_retweets(self, username):
+        int_post_id = int(self.post_id)
+        post = db["posts"].find_one({"_id": int_post_id})
+
+        if post is None:
+            return "Post not found."
+
+        if username not in post.get("retweeted_by", []):
+            db["posts"].update_one({"_id": int_post_id}, {"$inc": {"retweets": 1}, "$push": {"retweeted_by": username}})
+            db["users"].update_one(
+                {"username": username, "posts._id": int_post_id},
+                {"$inc": {"posts.$.retweets": 1}, "$push": {"retweeted_posts": int_post_id}}
+            )
+        else:
+            db["posts"].update_one({"_id": int_post_id},
+                                   {"$inc": {"retweets": -1}, "$pull": {"retweeted_by": username}})
+            db["users"].update_one(
+                {"username": username, "posts._id": int_post_id},
+                {"$inc": {"posts.$.retweets": -1}, "$pull": {"retweeted_posts": int_post_id}}
+            )
+        return None
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from flask import Flask, render_template, url_for, request, redirect, session
 
-import backend
+from backend import User, Post
 
 app = Flask(__name__)
 app.secret_key = "Halkd22f"
@@ -12,11 +12,11 @@ app.permanent_session_lifetime = timedelta(days=30)
 
 @app.route("/")
 def main():
-    posts = backend.get_all_posts()
+    posts = Post.get_all()
     if "user" in session:
         user = session["user"]
-        return render_template("index.html", user=user, posts=posts)
-    return render_template("index.html", posts=posts)
+        return render_template("index.html", user=user, posts=posts, get_username=get_username)
+    return render_template("index.html", posts=posts, get_username=get_username)
 
 
 def check_login(f):
@@ -35,13 +35,15 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        if backend.register_user(username, password):
+        user = User(username)
+        if user.register(password):
             session.permanent = True
             session["user"] = username
             return redirect(url_for("main"))
         else:
             return render_template("register.html",
-                                   error="The username is given or your username/password does not match the right pattern")
+                                   error="The username is given or your username/password does not match the right "
+                                         "pattern")
     elif request.method == "GET":
         if "user" in session:
             return redirect(url_for("main"))
@@ -53,7 +55,8 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        if backend.login_user(username, password):
+        user = User(username)
+        if user.login(password):
             session.permanent = True
             session["user"] = username
             return redirect(url_for("main"))
@@ -72,20 +75,37 @@ def logout():
     return redirect(url_for("main"))
 
 
+@app.route("/post")
+def user_post():
+    post_id = int(request.args.get('post_id'))
+    if post_id is None:
+        return "Post ID is missing!", 400
+    post_instance = Post(post_id)
+    post_details = post_instance.get_one()
+    comments = post_details["comments"]
+    return render_template("post.html", post=post_details, comments=comments)
+
+
 @app.route("/post", methods=["POST"])
 @check_login
 def post():
     if request.form["content"]:
-        backend.upload_message(session["user"], request.form["content"])
+        Post.upload_message(session["user"], request.form["content"])
     return redirect(url_for("main"))
+
+
+@app.route("/get_username/<user_id>")
+def get_username(user_id):
+    return User.get_username(user_id)
 
 
 @app.route("/profile")
 @check_login
 def profile():
     username = session["user"]
-    description = backend.get_description(username)
-    posts = backend.get_user_posts(username)
+    user = User(username)
+    description = user.get_description()
+    posts = Post.get_user(username)
     if posts == "TypeError":
         return render_template("index.html")
     return render_template("profile.html", username=username, posts=posts, description=description)
@@ -96,47 +116,41 @@ def profile():
 def edit_profile():
     if request.method == "GET":
         username = session["user"]
-        description = backend.get_description(username)
+        user = User(username)
+        description = user.get_description()
         return render_template("edit_profile.html", username=username, description=description)
     if request.method == "POST":
         username_old = session["user"]
         username_new = request.form["new_name"]
         description = request.form["new_description"]
-        if backend.edit_profile(username_old, username_new, description):
+        user = User(username_old)
+        if user.edit_profile(username_new, description):
             session["user"] = username_new
         return redirect(url_for("main"))
-
-
-
-@app.route("/post")
-def user_post():
-    post_id = request.args.get('post_id')
-    if post_id is None:
-        return "Post ID is missing!", 400
-    post = backend.get_one_post(post_id)
-    comments = post["comments"]
-    return render_template("post.html", post=post, comments=comments)
 
 
 @app.route("/likes/<post_id>")
 @check_login
 def likes(post_id):
-    backend.update_likes(post_id, session["user"])
+    post_instance = Post(post_id)
+    post_instance.update_likes(session["user"])
     return redirect(url_for("main"))
 
 
 @app.route("/retweet/<post_id>")
 @check_login
 def retweet(post_id):
-    backend.update_retweets(post_id, session["user"])
+    post_instance = Post(post_id)
+    post_instance.update_retweets(session["user"])
     return redirect(url_for("main"))
 
 
 @app.route("/comment", methods=["POST"])
 @check_login
 def comment():
-    backend.add_comment(request.form["post_id"], session["user"], request.form["comment_text"],
-                        request.form["post_creator"])
+    post_instance = Post(int(request.form["post_id"]))
+    post_instance.add_comment(session["user"], request.form["comment_text"],
+                              request.form["post_creator"])
     return redirect(url_for("main"))
 
 
